a527b3f83978d465559b508c3944077ee1058f69
Via <via.fairchild@gmail.com>
2025-06-12 00:24:03 -0600

    Website2.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://via-codes.github.io",
   integrations: [mdx(), sitemap(), tailwind(), react()],
 });
