931e63f01bff0519eb3539aa2f71aaccedc85eb9
Erick <65092726+savetent@users.noreply.github.com>
2025-06-12 01:05:52 -0600

    Website2.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://via-codes.github.io",
+  site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), sitemap(), tailwind(), react()],
 });
